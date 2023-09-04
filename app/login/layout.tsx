ba1d52d3414d2360513aaf1442e40cdadb0490e3
Vijayendher Gatla <117831928+wizaye@users.noreply.github.com>
2023-09-04 14:22:11 +0530

    Update layout.tsx

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -2,7 +2,9 @@
 import { SessionProvider } from "next-auth/react";
 
 export default function PostLayout({
-  children, // will be a page or nested layout
+ children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
 }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
